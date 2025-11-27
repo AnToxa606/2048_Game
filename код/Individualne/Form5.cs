9a37e027a0aaeb5fe5a63cb5b94effebf23bb9aa
Novza_Anton <Antonnovza@gmail.com>
2025-11-28 00:11:32 +0200

    New

--- код/Individualne/Form5.cs
+++ код/Individualne/Form5.cs
@@ -1,4 +1,54 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace Individualne
+//{
+//    public partial class PauseMenuForm: Form
+//    {
+//        public enum PauseAction { None, Restart, ExitToMenu }
+//        public PauseAction SelectedAction { get; private set; } = PauseAction.None;
+
+//        private Button btnContinue;
+//        private Button btnRestart;
+//        private Button btnExitToMenu;
+
+//        public PauseMenuForm()
+//        {
+//            this.Text = "Меню паузи";
+//            this.ClientSize = new Size(300, 250);
+//            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+//            this.MaximizeBox = false;
+//            this.StartPosition = FormStartPosition.CenterParent;
+
+//            btnContinue = new Button
+//            {
+//                Text = "Продовжити",
+//                Size = new Size(200, 40),
+//                Location = new Point(50, 30),
+//                Font = new Font("Segoe UI", 12, FontStyle.Bold)
+//            };
+//            btnContinue.Click += (s, e) => { this.DialogResult = DialogResult.Cancel; this.Close(); };
+
+//            btnRestart = new Button
+//            {
+//                Text = "Почати знову",
+
+//                Size = new Size(200, 40),
+//                Location = new Point(50, 90),
+//                Font = new Font("Segoe UI", 12, FontStyle.Bold)
+//            };
+//            btnRestart.Click += (s, e) => { SelectedAction = PauseAction.Restart; this.DialogResult = DialogResult.OK; this.Close(); };
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +60,7 @@ using System.Windows.Forms;
 
 namespace Individualne
 {
-    public partial class PauseMenuForm: Form
+    public partial class PauseMenuForm : Form
     {
         public enum PauseAction { None, Restart, ExitToMenu }
         public PauseAction SelectedAction { get; private set; } = PauseAction.None;
@@ -63,3 +113,21 @@ namespace Individualne
         }
     }
 }
+
+//            btnExitToMenu = new Button
+//            {
+//                Text = "Вийти в меню",
+//                Size = new Size(200, 40),
+//                Location = new Point(50, 150),
+//                Font = new Font("Segoe UI", 12, FontStyle.Bold)
+//            };
+//            btnExitToMenu.Click += (s, e) => { SelectedAction = PauseAction.ExitToMenu; this.DialogResult = DialogResult.OK; this.Close(); };
+
+//            this.Controls.Add(btnContinue);
+//            this.Controls.Add(btnRestart);
+//            this.Controls.Add(btnExitToMenu);
+
+//            ThemeManager.ApplyTheme(this);
+//        }
+//    }
+//}
